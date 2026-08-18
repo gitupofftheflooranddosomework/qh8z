7195aa1443496e2280a42687dd0b34c85b7bf56a
Mark Shaw <43359115+gitupofftheflooranddosomework@users.noreply.github.com>
2026-08-17 20:25:05 -0600

    Test mapped and reserved destination ranges

--- services/app/test/destination.test.mjs
+++ services/app/test/destination.test.mjs
@@ -4,5 +4,6 @@ import { assertDestinationAllowed } from '../src/destination.mjs';
 
 test('allows ordinary public destinations', () => assert.equal(assertDestinationAllowed('https://example.com/path'), 'https://example.com/path'));
 test('blocks localhost and local names', () => { assert.throws(() => assertDestinationAllowed('http://localhost/x')); assert.throws(() => assertDestinationAllowed('http://printer.local/x')); });
-test('blocks private and link-local IPv4 destinations', () => { for (const ip of ['127.0.0.1','10.1.2.3','172.16.1.1','192.168.1.1','169.254.169.254']) assert.throws(() => assertDestinationAllowed(`http://${ip}/`)); });
+test('blocks private, link-local, and reserved IPv4 destinations', () => { for (const ip of ['127.0.0.1','10.1.2.3','172.16.1.1','192.168.1.1','169.254.169.254','192.0.2.1','198.51.100.1','203.0.113.1']) assert.throws(() => assertDestinationAllowed(`http://${ip}/`)); });
 test('blocks loopback and unique-local IPv6 destinations', () => { assert.throws(() => assertDestinationAllowed('http://[::1]/')); assert.throws(() => assertDestinationAllowed('http://[fd00::1]/')); });
+test('blocks IPv4-mapped IPv6 literals that can hide private IPv4 targets', () => { assert.throws(() => assertDestinationAllowed('http://[::ffff:127.0.0.1]/')); assert.throws(() => assertDestinationAllowed('http://[::ffff:7f00:1]/')); });
