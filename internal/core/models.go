e1324f62febf0e3c61ac477386774e4af3ff4d01
Mark Shaw <43359115+gitupofftheflooranddosomework@users.noreply.github.com>
2026-08-18 03:08:10 -0600

    add core models

---
+++ internal/core/models.go
@@ -0,0 +1,29 @@
+package core
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrNotFound = errors.New("not found")
+	ErrConflict = errors.New("conflict")
+)
+
+type Link struct {
+	Slug string `json:"slug"`
+	URL string `json:"url"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type Visit struct {
+	Slug string `json:"-"`
+	VisitedAt time.Time `json:"visitedAt"`
+	Referer string `json:"referer,omitempty"`
+	UserAgent string `json:"userAgent,omitempty"`
+}
+
+type Stats struct {
+	TotalVisits int64 `json:"totalVisits"`
+	Recent []Visit `json:"recentVisits"`
+}
